b6acd38ff5ee3f5360c313d0de723c46b3be6c72
Joseph Fusco <jfusco@masonite.com>
2019-02-01 10:54:31 -0500

    Replace relative url image with absolute path image

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react'
 import { Button } from '@masonite/external-ui-react'
-import logo from './logo.svg'
+import styled from 'styled-components'
 import styles from './App.module.scss'
 
-const handleClick = () => {
-  alert('clicked!')
-}
-
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      clicks: 0,
+    }
+  }
+
+  IncrementItem = () => {
+    const { clicks } = this.state
+    this.setState({ clicks: clicks + 1 })
+  }
+
+  DecreaseItem = () => {
+    const { clicks } = this.state
+    this.setState({ clicks: clicks - 1 })
+  }
+
   render() {
+    const { clicks } = this.state
     return (
       <div className={styles.App}>
         <header className="App-header">
           <h1>Test React App</h1>
-          <img src={logo} className="App-logo" alt="logo" />
-          <Button variant="light" onClick={handleClick}>
-            click me
-          </Button>
+          <img
+            className="App-logo"
+            src="https://embed.widencdn.net/img/masonite/d58nubyyup/205px/Masonite_Logo_Stacked_1c_White.png"
+            alt="Masonite, open to extraordinary"
+          />
+          <h2>{clicks}</h2>
+          <ButtonGroup>
+            <Button variant="light" onClick={this.DecreaseItem}>
+              -
+            </Button>
+            <Button variant="light" onClick={this.IncrementItem}>
+              +
+            </Button>
+          </ButtonGroup>
         </header>
       </div>
     )
   }
 }
+
+const ButtonGroup = styled.div`
+  & > button + button {
+    margin-left: 5px;
+  }
+`
 
 export default App
